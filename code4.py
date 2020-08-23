b6fc42e97373b102c2f7ed9bdaa980c223564e52
eyshah <69476214+eyshah@users.noreply.github.com>
2020-08-23 20:21:01 +0100

    CODE 4 - Extract information using DictWriter.

---
+++ code4.py
@@ -0,0 +1,14 @@
+import csv
+
+csv_reader = csv.reader(open("boardmeetingnew.csv","r"), delimiter=",")
+
+with open("boardmeetingnew.csv", "r") as new_nfile:
+    csv_reader = csv.DictReader(new_nfile)
+
+    with open("new_meetingme2_csv", "w") as newest_nfile:
+        fieldnames =["First_name", "Last_name", "email"]
+
+        csv_writer = csv.DictWriter(newest_nfile, fieldnames=fieldnames, delimiter="\t")
+
+        for line in csv_reader:
+            csv_writer.writerow(line)
