bd980db0ac30c81c93f8dbc3d57844a373c3a73a
eyshah <69476214+eyshah@users.noreply.github.com>
2020-08-24 20:21:34 +0100

    Delete coding.py

--- coding.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open ("C:/Users/Ayesha's laptop/PycharmProjects/try-22/boardmeetingnew.csv","r") as csv_rfile:
-    csv_reader = csv.reader(csv_rfile)
-
-    for line in csv_rfile:
-        print(line)
