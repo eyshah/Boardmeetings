d9f144ef6c6cf7d39b0220f01bff26f4cff4d7ab
eyshah <69476214+eyshah@users.noreply.github.com>
2020-08-24 20:23:32 +0100

    CODE 2 - Write the same CVS file changing the delimiter.

---
+++ code2better.py
@@ -0,0 +1,10 @@
+import csv
+
+csv_reader= csv.reader(open("boardmeetingnew.csv","r"), delimiter=",")
+
+with open("new_meetingme_csv", "w") as new_nfile:
+     csv_writer = csv.writer(new_nfile, delimiter="/")
+
+     for row in csv_reader:
+        csv_writer.writerow(row)
+
