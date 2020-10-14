b364c6f15cc0b1ed4f9a3629121511ea261e087e
eyshah <69476214+eyshah@users.noreply.github.com>
2020-10-14 19:21:37 +0100

    Delete code2b.py

--- code2b.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-appendingcsv = open("new_meetingme.csv", "a")
-time = "4:30pm"
-appendingcsv.write("STELLA\nHARRIS\nTOM\n**have another team meating at**"  + str(time))
-appendingcsv.close()
