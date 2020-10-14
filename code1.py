e8527156e46cca877b0cf83ba0d131addc6e358b
eyshah <69476214+eyshah@users.noreply.github.com>
2020-10-14 19:23:28 +0100

    CODE 1 = How to open / read a csv file

---
+++ code1.py
@@ -0,0 +1,9 @@
+# Imports the csv module
+import csv
+# opens the csv file to "r" read function
+with open("C:/Users/Ayesha's laptop/PycharmProjects/try-22/boardmeetingnew.csv","r") as csv_rfile:
+    # Variable created to store the file in
+    to_extract = csv.reader(csv_rfile)
+    # final command will print the rows as an output from the "csv_rfile"
+    for row in csv_rfile:
+        print(row)
