2b7fab84698701afd586926cb559585aa185f932
eyshah <69476214+eyshah@users.noreply.github.com>
2020-10-14 19:21:52 +0100

    Delete code3.py

--- code3.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-reader = csv.reader(open("boardmeetingnew.csv","r"), delimiter=",")
-
-with open("boardmeetingnew.csv", "r") as new_nfile:
-    reading = csv.DictReader(new_nfile)
-
-    for row in reading:
-        print(row["email"])
