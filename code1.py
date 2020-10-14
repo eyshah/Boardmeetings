8f26337a622aa5f071f6d24b622ef7fbb21f3778
eyshah <69476214+eyshah@users.noreply.github.com>
2020-10-14 19:21:10 +0100

    Delete code1.py

--- code1.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open("C:/Users/Ayesha's laptop/PycharmProjects/try-22/boardmeetingnew.csv","r") as csv_rfile:
-    to_extract = csv.reader(csv_rfile)
-
-    for row in csv_rfile:
-        print(row)
-
