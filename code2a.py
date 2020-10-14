bb96e000bf6f34027791f6682ddccccfcae68ad7
eyshah <69476214+eyshah@users.noreply.github.com>
2020-10-14 19:21:25 +0100

    Delete code2a.py

--- code2a.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-csvreadings = csv.reader(open("boardmeetingnew.csv", "r"), delimiter=",")
-
-with open("new_meetingme.csv", "w") as new_nfile:
-    csv_writing = csv.writer(new_nfile, delimiter="/")
-
-    for row in csvreadings:
-        csv_writing.writerow(row)
